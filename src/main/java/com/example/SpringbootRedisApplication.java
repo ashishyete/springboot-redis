1c0a63ce629261cc131398cf0eb90cea86a24b32
Ashish Yete <ashish.yete@gmail.com>
2021-08-16 15:18:21 +0530

    second commit

--- src/main/java/com/example/SpringbootRedisApplication.java
+++ src/main/java/com/example/SpringbootRedisApplication.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class SpringbootRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRedisApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void greet() {
+		System.out.println("Hello Greetings");
 	}
 
 }
